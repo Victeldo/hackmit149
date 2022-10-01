8376b8620d671654534b5eeaa047af8acc93d98f
Prithvi <44360797+Victeldo@users.noreply.github.com>
2022-10-01 16:49:51 -0400

    Added autocomplete address
    Input box uses Google Maps API to autofill the address.

--- sustainable/src/App.js
+++ sustainable/src/App.js
@@ -1,25 +1,28 @@
-import logo from "./logo.svg";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 
-function App() {
+const AutoComplete = () => {
+  const autoCompleteRef = useRef();
+  const inputRef = useRef();
+  const options = {
+    fields: ["address_components", "geometry", "icon", "name"],
+  };
+  useEffect(() => {
+    autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+      inputRef.current,
+      options
+    );
+    autoCompleteRef.current.addListener("place_changed", async function () {
+      const place = await autoCompleteRef.current.getPlace();
+      console.log({ place });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>enter address :</label>
+      <input ref={inputRef} />
     </div>
   );
-}
-
-export default App;
+};
+export default AutoComplete;
