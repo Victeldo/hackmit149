b16ea2ae21528dc5eece445b2f57d4e185ff7e99
Prithvi <44360797+Victeldo@users.noreply.github.com>
2022-10-02 01:17:25 -0400

    Implemented the distance
    Used https://github.com/manuelbieh/geolib
    
    Based on whatever is filled in the google maps API autocorrect address field, it calculates the geographic distance from that point to the Greater Boston Food Bank

--- sustainable/src/App.js
+++ sustainable/src/App.js
@@ -1,7 +1,9 @@
 import React, { useRef, useEffect } from "react";
+import { getPreciseDistance } from "geolib";
 import "./App.css";
 
 const AutoComplete = () => {
+  var loc = " ";
   const autoCompleteRef = useRef();
   const inputRef = useRef();
   const options = {
@@ -14,14 +16,34 @@ const AutoComplete = () => {
     );
     autoCompleteRef.current.addListener("place_changed", async function () {
       const place = await autoCompleteRef.current.getPlace();
-      console.log({ place });
+      loc = { place };
     });
   }, []);
+
+  function findDist() {
+    console.log(loc.place.geometry["location"].lat());
+    console.log(loc.place.geometry["location"].lng());
+    console.log(
+      getPreciseDistance(
+        { latitude: 42.3344051, longitude: -71.0658327 }, //Food bank placeholder coords
+        {
+          latitude: loc.place.geometry["location"].lat(),
+          longitude: loc.place.geometry["location"].lng(),
+        }
+      )
+    );
+  }
 
   return (
     <div>
       <label>enter address :</label>
-      <input ref={inputRef} />
+      <input
+        ref={inputRef}
+        onChange={(evt) => {
+          loc = evt.target.value;
+        }}
+      />
+      <button onClick={findDist}>Calculate Distance to Food Bank</button>
     </div>
   );
 };
